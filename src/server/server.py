4451435b7dce493f338895d886f180aea9b3c848
devinda_121260 <devinda.weerapperuma@axiatadigitallabs.com>
2024-08-21 08:24:38 +0530

    cors added.

--- src/server/server.py
+++ src/server/server.py
@@ -1,14 +1,15 @@
 from flask import Flask, jsonify, request
+from flask_cors import CORS
 
 
 class FlaskServer:
     def __init__(self, output_svg_path, output_json_path, svg_processor):
         self.app = Flask(__name__)
+        CORS(self.app)  # Enable CORS for all routes
         self._configure_routes()
         self._svg_path = output_svg_path
         self._output_json_path = output_json_path
         self._svg_processor = svg_processor
-        # Define a route to serve an SVG file
 
     def _configure_routes(self):
         # Define a route to accept and process DOT content from the request body
@@ -33,7 +34,7 @@ class FlaskServer:
             except FileNotFoundError:
                 return jsonify(error="SVG file not found"), 404
 
-        # Define a route to serve an SVG file
+        # Define a route to serve a JSON file
         @self.app.route('/output/json', methods=['GET'])
         def serve_json():
             try:
@@ -43,5 +44,5 @@ class FlaskServer:
             except FileNotFoundError:
                 return jsonify(error="SVG file not found"), 404
 
-    def run(self, host='0.0.0.0', port=5000, debug=False):
+    def run(self, host='0.0.0.0', port=5500, debug=False):
         self.app.run(host=host, port=port, debug=debug)
